a89eb456f24e9fff00a60c8485753f714130d1d1
Carlos Vinicios <carlos.martins@nca.ufma.br>
2019-05-15 10:07:54 -0300

    Trabalho 1

---
+++ MovingThrough/Assets/CubeInteraction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeInteraction : MonoBehaviour
+{
+    Material localMat;
+    MeshRenderer mRender;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mRender = GetComponent<MeshRenderer>();
+        localMat = mRender.material;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Gaze Interactions
+    public void OnGazeEnter(){
+        localMat.color = Color.blue;
+    }
+
+    public void OnGazeExit(){
+        localMat.color = Color.white;
+    }
+}
---
+++ MovingThrough/Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 3.0f;
+    Rigidbody rbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rbody.velocity = Vector3.zero;
+        rbody.angularVelocity = Vector3.zero;
+    }
+
+    void FixedUpdate()
+    {
+        if(Input.touchCount > 0 || Input.GetMouseButton(0))//move body
+        {
+            rbody.MovePosition(transform.position+transform.forward*Time.deltaTime*speed);
+        }
+    }
+}
